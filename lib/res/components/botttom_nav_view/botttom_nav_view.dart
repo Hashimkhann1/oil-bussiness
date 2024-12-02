ccb1ce3f311d0fb18563a483172fc72ee8b67883
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-12-02 21:41:37 +0500

    bottom nav file

---
+++ lib/res/components/botttom_nav_view/botttom_nav_view.dart
@@ -0,0 +1,12 @@
+
+import '../../paths/paths.dart';
+
+
+class BottomNavView extends StatelessWidget {
+  const BottomNavView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
