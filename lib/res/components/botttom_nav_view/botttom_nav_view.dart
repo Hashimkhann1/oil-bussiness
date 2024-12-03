5aa6165d3ad17a2eccad10fcc3544f4b97f92607
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-12-03 23:00:41 +0500

    completed

--- lib/res/components/botttom_nav_view/botttom_nav_view.dart
+++ lib/res/components/botttom_nav_view/botttom_nav_view.dart
@@ -1,12 +1,66 @@
 
+import 'package:flutter/cupertino.dart';
 import '../../paths/paths.dart';
 
 
-class BottomNavView extends StatelessWidget {
+class BottomNavView extends StatefulWidget {
   const BottomNavView({super.key});
 
   @override
+  State<BottomNavView> createState() => _BottomNavViewState();
+}
+
+class _BottomNavViewState extends State<BottomNavView> {
+
+  int selectedView = 0;
+
+  List view = [
+    AdminHomeView(),
+    AdminAllCustomersView(),
+    AdminAddRole(),
+    AdminProfileView()
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocBuilder<UserDataBloc , UserDataBlocState>(
+        builder: (context , state) {
+          if(state.userDataist.isEmpty){
+            return Scaffold(
+              body: Center(
+                child: Container(
+                  child: CircularProgressIndicator(color: AppColor.primaryColor,),
+                ),
+              ),
+            );
+          }else{
+            if(state.userDataist[0].accountRole == 'initial'){
+              return InitialUserView();
+            }else{
+              return state.userDataist[0].accountRole == 'admin' ? Scaffold(
+                body: view[selectedView],
+                bottomNavigationBar: BottomNavigationBar(
+                  type: BottomNavigationBarType.fixed,
+                  currentIndex: selectedView,
+                  elevation: 0,
+                  selectedItemColor: AppColor.black,
+                  onTap: (index) {
+                    setState(() {
+                      selectedView = index;
+                    });
+                  },
+                  items: [
+                    BottomNavigationBarItem(icon: Icon(Icons.home,size: 32,),label: ''),
+                    BottomNavigationBarItem(icon: Icon(Icons.people,size: 32,),label: ''),
+                    BottomNavigationBarItem(icon: Icon(Icons.add,size: 32,),label: ''),
+                    BottomNavigationBarItem(icon: Icon(CupertinoIcons.person_alt_circle_fill,size: 32,),label: ''),
+                  ],
+                  selectedIconTheme: IconThemeData(color: AppColor.primaryColor),
+                ),
+              ) : Scaffold();
+            }
+          }
+        }
+    );
   }
 }
