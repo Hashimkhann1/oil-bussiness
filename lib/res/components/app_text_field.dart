b4876980bec405963e4b26e2ad3ed34b157df2d0
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-12-03 23:04:37 +0500

    colores , components and themes is added

---
+++ lib/res/components/app_text_field.dart
@@ -0,0 +1,69 @@
+
+import '../paths/paths.dart';
+
+class AppTextField extends StatelessWidget {
+
+  const AppTextField({
+    required this.hintText,
+    this.controller,
+    this.validator,
+    this.keyboardType,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.onChanged,
+    this.onFieldSubmitted,
+    this.obscureText = false,
+    this.inputFormatters,
+    this.enable = true,
+    this.width = 300,
+    this.height = 35,
+    this.fontSize = 12,
+    super.key});
+
+
+  final String hintText;
+  final TextEditingController? controller;
+  final String? Function(String?)? validator;
+  final TextInputType? keyboardType;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+  final void Function(String)? onChanged;
+  final void Function(String)? onFieldSubmitted;
+  final bool obscureText;
+  final bool enable;
+  final List<TextInputFormatter>? inputFormatters;
+  final double width;
+  final double height;
+  final double fontSize;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      child: TextFormField(
+        controller: controller,
+        validator: validator,
+        keyboardType: keyboardType,
+        inputFormatters: inputFormatters,
+        cursorHeight: 12,
+        cursorColor: AppColor.primaryColor,
+        style: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: fontSize,
+        ),
+        decoration: InputDecoration(
+          hintText: hintText,
+          suffixIcon: suffixIcon,
+          prefixIcon: prefixIcon,
+        ),
+        onChanged: onChanged,
+        enabled: enable,
+        onFieldSubmitted: onFieldSubmitted,
+        obscureText: obscureText,
+      ),
+    );
+  }
+}
