54ecfbe8c916cac8c20796e038928b2919c9b509
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-12-10 09:25:00 +0500

    authentication

--- lib/view/authentication/login_view/login_view.dart
+++ lib/view/authentication/login_view/login_view.dart
@@ -18,121 +18,127 @@ class LoginView extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: AppColor.white,
-      body: Center(
-        child: Container(
-          width: width * 0.96,
-          height: height * 0.44,
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-              color: AppColor.white,
-              borderRadius: BorderRadius.circular(8),
-              boxShadow: [
-                BoxShadow(
-                    color: AppColor.black.withOpacity(0.2),
-                    spreadRadius: 4,
-                    blurRadius: 10)
-              ]),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: height * 0.01,
-              ),
+      body: PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (didPop , result) {
+          SystemNavigator.pop();
+        },
+        child: Center(
+          child: Container(
+            width: width * 0.96,
+            height: height * 0.44,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: AppColor.white,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                      color: AppColor.black.withOpacity(0.2),
+                      spreadRadius: 4,
+                      blurRadius: 10)
+                ]),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: height * 0.01,
+                ),
 
-              /// jan Ghani Name
-              const MyText(
-                title: "POS App",
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                color: AppColor.primaryColor,
-              ),
-              SizedBox(
-                height: height * 0.08,
-              ),
-
-              /// form
-              Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      AppTextField(
-                        controller: emailController,
-                        hintText: "Email",
-                        width: 390,
-                        height: height * 0.05,
-                        fontSize: 17,
-                        validator: (val) {
-                          if (val!.isEmpty) {
-                            return "Enter email";
-                          } else if (!val.contains('@gmail.com')) {
-                            return "Enter valid email";
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: height * 0.01,
-                      ),
-                      AppTextField(
-                        controller: passwordController,
-                        obscureText: true,
-                        hintText: "password",
-                        width:390,
-                        height: height * 0.05,
-                        fontSize: 17,
-                        validator: (val) {
-                          if (val!.isEmpty) {
-                            return "Enter Password";
-                          } else if (val.length < 7) {
-                            return "Password must be grater than 7 characters";
-                          }
-                          return null;
-                        },
-                      )
-                    ],
-                  )),
-              SizedBox(
-                height: height * 0.02,
-              ),
-
-              /// sign in  button
-              BlocBuilder<LoadingBloc , LoadingBlocState>(builder: (context , state) {
-                return AppTextButton(
-                  title: "Log In",
-                  textColor: AppColor.white,
-                  fontSize: 19,
+                /// jan Ghani Name
+                const MyText(
+                  title: "POS App",
+                  fontSize: 40,
                   fontWeight: FontWeight.bold,
-                  backgroundColor: AppColor.primaryColor,
-                  width: 170,
-                  height: 50,
-                  borderRadius: 22,
-                  isLoading: state.isLoading,
-                  onTap: () {
-                    if(_formKey.currentState!.validate()){
-                      _authenticationViewModel.signIn(context, emailController.text.toString(), passwordController.text.toString());
-                    }
-                  },
-                );
-              }),
+                  color: AppColor.primaryColor,
+                ),
+                SizedBox(
+                  height: height * 0.08,
+                ),
 
-              const Spacer(),
-              Align(
-                  alignment: Alignment.bottomRight,
-                  child: InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SignupView()));
-                      },
-                      child: const MyText(
-                        title: "Don't have an account",
-                        fontSize: 16,
-                        color: Colors.blueAccent,
-                        decoration: TextDecoration.underline,
-                      )))
-            ],
+                /// form
+                Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        AppTextField(
+                          controller: emailController,
+                          hintText: "Email",
+                          width: 390,
+                          height: height * 0.05,
+                          fontSize: 17,
+                          validator: (val) {
+                            if (val!.isEmpty) {
+                              return "Enter email";
+                            } else if (!val.contains('@gmail.com')) {
+                              return "Enter valid email";
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(
+                          height: height * 0.01,
+                        ),
+                        AppTextField(
+                          controller: passwordController,
+                          obscureText: true,
+                          hintText: "password",
+                          width:390,
+                          height: height * 0.05,
+                          fontSize: 17,
+                          validator: (val) {
+                            if (val!.isEmpty) {
+                              return "Enter Password";
+                            } else if (val.length < 7) {
+                              return "Password must be grater than 7 characters";
+                            }
+                            return null;
+                          },
+                        )
+                      ],
+                    )),
+                SizedBox(
+                  height: height * 0.02,
+                ),
+
+                /// sign in  button
+                BlocBuilder<LoadingBloc , LoadingBlocState>(builder: (context , state) {
+                  return AppTextButton(
+                    title: "Log In",
+                    textColor: AppColor.white,
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold,
+                    backgroundColor: AppColor.primaryColor,
+                    width: 170,
+                    height: 50,
+                    borderRadius: 22,
+                    isLoading: state.isLoading,
+                    onTap: () {
+                      if(_formKey.currentState!.validate()){
+                        _authenticationViewModel.signIn(context, emailController.text.toString(), passwordController.text.toString());
+                      }
+                    },
+                  );
+                }),
+
+                const Spacer(),
+                Align(
+                    alignment: Alignment.bottomRight,
+                    child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignupView()));
+                        },
+                        child: const MyText(
+                          title: "Don't have an account",
+                          fontSize: 16,
+                          color: Colors.blueAccent,
+                          decoration: TextDecoration.underline,
+                        )))
+              ],
+            ),
           ),
         ),
       ),
--- lib/view/authentication/signin_up_view/sign_up_view.dart
+++ lib/view/authentication/signin_up_view/sign_up_view.dart
@@ -20,139 +20,145 @@ class SignupView extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: AppColor.white,
-      body: Center(
-        child: Container(
-          width: width * 0.96,
-          height: height * 0.59,
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-              color: AppColor.white,
-              borderRadius: BorderRadius.circular(8),
-              boxShadow: [
-                BoxShadow(
-                    color: AppColor.primaryColor.withOpacity(0.2),
-                    spreadRadius: 4,
-                    blurRadius: 10)
-              ]),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: height * 0.01,
-              ),
+      body: PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (didPop , result) {
+          SystemNavigator.pop();
+        },
+        child: Center(
+          child: Container(
+            width: width * 0.96,
+            height: height * 0.59,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: AppColor.white,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                      color: AppColor.primaryColor.withOpacity(0.2),
+                      spreadRadius: 4,
+                      blurRadius: 10)
+                ]),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: height * 0.01,
+                ),
 
-              /// janGhani Name
-              const MyText(
-                title: "POS App",
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                color: AppColor.primaryColor,
-              ),
-              SizedBox(
-                height: height * 0.04,
-              ),
-
-              /// form
-              Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      AppTextField(
-                        controller: emailController,
-                        hintText: "Email",
-                        width: 390,
-                        height: 60,
-                        fontSize: 17,
-                        validator: (val){
-                          if(val!.isEmpty){
-                            return "Enter email";
-                          }else if(!val.contains('@gmail.com')){
-                            return "Enter valid email";
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: height * 0.01,
-                      ),
-                      AppTextField(
-                        controller: passwordController,
-                        obscureText: true,
-                        hintText: "Password",
-                        width: 390,
-                        height: 60,
-                        fontSize: 17,
-                        validator: (val){
-                          if(val!.isEmpty){
-                            return "Enter Password";
-                          }else if(val.length < 7){
-                            return "Password must be grater than 7 characters";
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: height * 0.01,
-                      ),
-                      AppTextField(
-                        controller: confirmPasswordController,
-                        obscureText: true,
-                        hintText: "Confirm password",
-                        width: 390,
-                        height: 60,
-                        fontSize: 17,
-                        validator: (val){
-                          if(val!.isEmpty){
-                            return "Enter Confirm Password";
-                          }
-                          else if(passwordController.text != val){
-                            return "Confirm password not matched!";
-                          }
-                          return null;
-                        },
-                      )
-                    ],
-                  )),
-              SizedBox(
-                height: height * 0.03,
-              ),
-
-              /// sign in  button
-              BlocBuilder<LoadingBloc , LoadingBlocState>(builder: (context , state){
-                return AppTextButton(
-                  title: "Sign Up",
-                  textColor: AppColor.white,
-                  fontSize: 19,
+                /// janGhani Name
+                const MyText(
+                  title: "POS App",
+                  fontSize: 40,
                   fontWeight: FontWeight.bold,
-                  backgroundColor: AppColor.primaryColor,
-                  width: 170,
-                  height: 50,
-                  borderRadius: 22,
-                  isLoading: state.isLoading,
-                  onTap: () {
-                    if(_formKey.currentState!.validate()){
-                      print("Working >>>>>>>>>>>>>>");
-                      _authenticationViewModel.signUp(context, emailController.text.toString(), passwordController.text.toString());
-                    }
-                  },
-                );
-              }),
+                  color: AppColor.primaryColor,
+                ),
+                SizedBox(
+                  height: height * 0.04,
+                ),
 
-              const Spacer(),
-              Align(
-                  alignment: Alignment.bottomRight,
-                  child: InkWell(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: const MyText(
-                        title: "Already have an account",
-                        fontSize: 16,
-                        color: Colors.blueAccent,
-                        decoration: TextDecoration.underline,
-                      )))
-            ],
+                /// form
+                Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        AppTextField(
+                          controller: emailController,
+                          hintText: "Email",
+                          width: 390,
+                          height: 60,
+                          fontSize: 17,
+                          validator: (val){
+                            if(val!.isEmpty){
+                              return "Enter email";
+                            }else if(!val.contains('@gmail.com')){
+                              return "Enter valid email";
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(
+                          height: height * 0.01,
+                        ),
+                        AppTextField(
+                          controller: passwordController,
+                          obscureText: true,
+                          hintText: "Password",
+                          width: 390,
+                          height: 60,
+                          fontSize: 17,
+                          validator: (val){
+                            if(val!.isEmpty){
+                              return "Enter Password";
+                            }else if(val.length < 7){
+                              return "Password must be grater than 7 characters";
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(
+                          height: height * 0.01,
+                        ),
+                        AppTextField(
+                          controller: confirmPasswordController,
+                          obscureText: true,
+                          hintText: "Confirm password",
+                          width: 390,
+                          height: 60,
+                          fontSize: 17,
+                          validator: (val){
+                            if(val!.isEmpty){
+                              return "Enter Confirm Password";
+                            }
+                            else if(passwordController.text != val){
+                              return "Confirm password not matched!";
+                            }
+                            return null;
+                          },
+                        )
+                      ],
+                    )),
+                SizedBox(
+                  height: height * 0.03,
+                ),
+
+                /// sign in  button
+                BlocBuilder<LoadingBloc , LoadingBlocState>(builder: (context , state){
+                  return AppTextButton(
+                    title: "Sign Up",
+                    textColor: AppColor.white,
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold,
+                    backgroundColor: AppColor.primaryColor,
+                    width: 170,
+                    height: 50,
+                    borderRadius: 22,
+                    isLoading: state.isLoading,
+                    onTap: () {
+                      if(_formKey.currentState!.validate()){
+                        print("Working >>>>>>>>>>>>>>");
+                        _authenticationViewModel.signUp(context, emailController.text.toString(), passwordController.text.toString());
+                      }
+                    },
+                  );
+                }),
+
+                const Spacer(),
+                Align(
+                    alignment: Alignment.bottomRight,
+                    child: InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const MyText(
+                          title: "Already have an account",
+                          fontSize: 16,
+                          color: Colors.blueAccent,
+                          decoration: TextDecoration.underline,
+                        )))
+              ],
+            ),
           ),
         ),
       ),
