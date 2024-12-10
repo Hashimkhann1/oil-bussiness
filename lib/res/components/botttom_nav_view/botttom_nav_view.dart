84d020d42d518ef29b7e9dcf8f0a08ef7f9edef2
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-12-10 09:22:26 +0500

    updated

--- lib/res/components/botttom_nav_view/botttom_nav_view.dart
+++ lib/res/components/botttom_nav_view/botttom_nav_view.dart
@@ -1,7 +1,8 @@
-
 import 'package:flutter/cupertino.dart';
+import 'package:oil_bussiness/view/admin_view/all_stock_and_poducts_view/all_stock_and_poducts_view.dart';
+import 'package:oil_bussiness/view/markiting_view/all_customer_view/all_customer_view.dart';
+import 'package:oil_bussiness/view/markiting_view/setting_view/setting_view.dart';
 import '../../paths/paths.dart';
-
 
 class BottomNavView extends StatefulWidget {
   const BottomNavView({super.key});
@@ -11,56 +12,98 @@ class BottomNavView extends StatefulWidget {
 }
 
 class _BottomNavViewState extends State<BottomNavView> {
-
   int selectedView = 0;
 
-  List view = [
+  final List<Widget> adminView = [
     AdminHomeView(),
     AdminAllCustomersView(),
     AdminAddRole(),
-    AdminProfileView()
+    AdminProfileView(),
+  ];
+
+  final List<Widget> marketerView = [
+    AllCustomerView(),
+    MarketerAddCustomerView(),
+    SettingView(),
+    MarketerProfileView(),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<UserDataBloc , UserDataBlocState>(
-        builder: (context , state) {
-          if(state.userDataist.isEmpty){
-            return Scaffold(
-              body: Center(
-                child: Container(
-                  child: CircularProgressIndicator(color: AppColor.primaryColor,),
-                ),
-              ),
-            );
-          }else{
-            if(state.userDataist[0].accountRole == 'initial'){
-              return InitialUserView();
-            }else{
-              return state.userDataist[0].accountRole == 'admin' ? Scaffold(
-                body: view[selectedView],
-                bottomNavigationBar: BottomNavigationBar(
-                  type: BottomNavigationBarType.fixed,
-                  currentIndex: selectedView,
-                  elevation: 0,
-                  selectedItemColor: AppColor.black,
-                  onTap: (index) {
-                    setState(() {
-                      selectedView = index;
-                    });
-                  },
-                  items: [
-                    BottomNavigationBarItem(icon: Icon(Icons.home,size: 32,),label: ''),
-                    BottomNavigationBarItem(icon: Icon(Icons.people,size: 32,),label: ''),
-                    BottomNavigationBarItem(icon: Icon(Icons.add,size: 32,),label: ''),
-                    BottomNavigationBarItem(icon: Icon(CupertinoIcons.person_alt_circle_fill,size: 32,),label: ''),
-                  ],
-                  selectedIconTheme: IconThemeData(color: AppColor.primaryColor),
-                ),
-              ) : Scaffold();
-            }
-          }
+    return BlocBuilder<UserDataBloc, UserDataBlocState>(
+      builder: (context, state) {
+        if (state.userDataist.isEmpty) {
+          return _buildLoadingView();
         }
+
+        if (state.userDataist[0].accountRole == 'initial') {
+          return InitialUserView();
+        }
+
+        // final isAdmin = state.userDataist[0].accountRole == 'admin';
+        return _buildScaffold(
+          state.userDataist[0].accountRole == 'admin' ? adminView : marketerView,
+          state.userDataist[0].accountRole == 'admin' ? _adminBottomNavItems() : _marketerBottomNavItems(),
+        );
+      },
     );
+  }
+
+  Widget _buildLoadingView() {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColor.primaryColor,
+        title: MyText(title: "Loadin...",fontWeight: FontWeight.bold,color: AppColor.white,),
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  Scaffold _buildScaffold(List<Widget> views, List<BottomNavigationBarItem> items) {
+    return Scaffold(
+      body: views[selectedView],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: AppColor.primaryColor.withOpacity(0.3),
+              blurRadius: 10,
+              spreadRadius: 4
+            )
+          ]
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: selectedView,
+          elevation: 10,
+          selectedItemColor: AppColor.black,
+          selectedLabelStyle: TextStyle(color: AppColor.primaryColor,fontWeight: FontWeight.bold,fontSize: 12),
+          onTap: (index) => setState(() => selectedView = index),
+          items: items,
+          selectedIconTheme: const IconThemeData(color: AppColor.primaryColor),
+        ),
+      ),
+    );
+  }
+
+  List<BottomNavigationBarItem> _adminBottomNavItems() {
+    return const [
+      BottomNavigationBarItem(icon: Icon(Icons.home, size: 32), label: 'Dashboard'),
+      BottomNavigationBarItem(icon: Icon(Icons.people, size: 32), label: 'Customers'),
+      BottomNavigationBarItem(icon: Icon(Icons.add, size: 32), label: 'Assign Role'),
+      BottomNavigationBarItem(icon: Icon(CupertinoIcons.person_alt_circle_fill, size: 32), label: 'Profile'),
+    ];
+  }
+
+  List<BottomNavigationBarItem> _marketerBottomNavItems() {
+    return const [
+      BottomNavigationBarItem(icon: Icon(Icons.people, size: 32), label: 'Customers'),
+      BottomNavigationBarItem(icon: Icon(Icons.person_add, size: 32), label: 'add Customer'),
+      BottomNavigationBarItem(icon: Icon(Icons.settings, size: 32), label: 'Setting'),
+      BottomNavigationBarItem(icon: Icon(CupertinoIcons.person_alt_circle_fill, size: 32), label: 'Profile'),
+    ];
   }
 }
