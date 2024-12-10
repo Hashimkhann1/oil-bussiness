1f43e19110b931d9700adbf61833cda25a85955d
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-12-10 09:22:45 +0500

    admin add role view

--- lib/view/admin_view/admin_add_role_view/admin_add_role.dart
+++ lib/view/admin_view/admin_add_role_view/admin_add_role.dart
@@ -1,10 +1,6 @@
-
-
-import 'package:flutter/material.dart';
-import 'package:oil_bussiness/res/color/app_color.dart';
-import 'package:oil_bussiness/res/components/app_text_button.dart';
-import 'package:oil_bussiness/res/components/app_text_field.dart';
-import 'package:oil_bussiness/res/components/my_text.dart';
+import 'package:oil_bussiness/res/paths/paths.dart';
+import 'package:oil_bussiness/res/paths/paths.dart';
+import 'package:oil_bussiness/view_model/admin_view_model/assign_role_view_model/assign_role_view_model.dart';
 
 class AdminAddRole extends StatefulWidget {
   const AdminAddRole({super.key});
@@ -14,52 +10,101 @@ class AdminAddRole extends StatefulWidget {
 }
 
 class _AdminAddRoleState extends State<AdminAddRole> {
-
+  final userEmailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   String selectedValue = "Marketing";
 
   @override
   Widget build(BuildContext context) {
-
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColor.primaryColor,
-        automaticallyImplyLeading: false,
-        title: MyText(title: "Assign Role",fontWeight: FontWeight.w900,color: AppColor.white,),
-        iconTheme: IconThemeData(color: AppColor.white),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            MyText(title: "Assign Role",fontSize: 24,fontWeight: FontWeight.w900,color: AppColor.primaryColor,),
-            SizedBox(height: height * 0.06,),
-
-            AppTextField(hintText: "Employ Email",fontSize: 17,),
-
-            SizedBox(height: height * 0.03,),
-
-            DropdownButton(
-                items: const [
-                  DropdownMenuItem(value: "Marketing",child: MyText(title:"Marketing"),),
-                  DropdownMenuItem(value: "Purchasing or Recovery",child: MyText(title:"Purchasing or Recovery",),)
-                ],
-                value: selectedValue,
-                onChanged: (newVaue) {
-                  setState(() {
-                    selectedValue = newVaue.toString();
-                  });
-                }),
-
-            SizedBox(height: 20,),
-            
-            AppTextButton(title: "Add",fontSize: 20,fontWeight: FontWeight.bold,width: width * 0.4,height: 44,backgroundColor: AppColor.primaryColor,textColor: AppColor.white,borderRadius: 22,)
-          ],
+        appBar: AppBar(
+          backgroundColor: AppColor.primaryColor,
+          automaticallyImplyLeading: false,
+          title: MyText(
+            title: "Assign Role",
+            fontWeight: FontWeight.w900,
+            color: AppColor.white,
+          ),
+          iconTheme: IconThemeData(color: AppColor.white),
         ),
-      )
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              MyText(
+                title: "Assign Role",
+                fontSize: 24,
+                fontWeight: FontWeight.w900,
+                color: AppColor.primaryColor,
+              ),
+              SizedBox(
+                height: height * 0.06,
+              ),
+              Form(
+                  key: _formKey,
+                  child: AppTextField(
+                    controller: userEmailController,
+                    hintText: "Employ Email",
+                    fontSize: 17,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Enter Employee Email";
+                      }
+                      return null;
+                    },
+                  )),
+              SizedBox(
+                height: height * 0.03,
+              ),
+              DropdownButton(
+                  items: const [
+                    DropdownMenuItem(
+                      value: "Marketing",
+                      child: MyText(title: "Marketing"),
+                    ),
+                    DropdownMenuItem(
+                      value: "Purchasing or Recovery",
+                      child: MyText(
+                        title: "Purchasing or Recovery",
+                      ),
+                    )
+                  ],
+                  value: selectedValue,
+                  onChanged: (newVaue) {
+                    setState(() {
+                      selectedValue = newVaue.toString();
+                    });
+                  }),
+              SizedBox(
+                height: 20,
+              ),
+              BlocBuilder<LoadingBloc, LoadingBlocState>(
+                builder: (context, state) {
+                  return AppTextButton(
+                    title: "Add",
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    width: width * 0.4,
+                    height: 44,
+                    backgroundColor: AppColor.primaryColor,
+                    textColor: AppColor.white,
+                    borderRadius: 22,
+                    isLoading: state.isLoading,
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {
+                        AssignRoleViewModel().assignRole(
+                            context, userEmailController, selectedValue);
+                      }
+                    },
+                  );
+                },
+              )
+            ],
+          ),
+        ));
   }
 }
